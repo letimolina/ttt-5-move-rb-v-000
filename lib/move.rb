ab800663dbd7aeaf6003bc56ead9b290ccf279f8
Leticia Molina <letimolina@gmail.com>
2018-04-21 18:05:43 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+# board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +8,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i - 1
+end
+  
+def move(array, index, character = "X")
+  array[index] = character
+end
+
+# move(board, 0)
+# display_board(board)
+# # The element at index 0 of array 'board' is set to the value "X"
+# board #=> ["X", " ", " "]
